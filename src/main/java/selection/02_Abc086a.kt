37cd07424d658e5debd9285c5645ae1990284394
luecy1 <kowaheno@gmail.com>
2019-08-31 10:52:47 +0900

    AtCoder Beginners Selection 02 問題番号の修正

--- src/main/java/selection/02_Abc086a.kt
+++ src/main/java/selection/02_Abc086a.kt
@@ -4,10 +4,10 @@ fun main(args: Array<String>) {
     val sc = Scanner(System.`in`)
     val (a, b) = sc.nextLine().split(" ").map(String::toInt)
 
-    println(abc_186a(a, b))
+    println(abc_086a(a, b))
 }
 
-fun abc_186a(a: Int, b: Int): String {
+fun abc_086a(a: Int, b: Int): String {
     return if (a * b % 2 == 0) {
         "Even"
     } else {
