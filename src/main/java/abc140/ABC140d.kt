d851d4a55833ea11539e7f159283cbea35f36e3f
luecy1 <kowaheno@gmail.com>
2019-09-08 16:01:23 +0900

    ABC140 D問題を修正

--- src/main/java/abc140/ABC140d.kt
+++ src/main/java/abc140/ABC140d.kt
@@ -7,14 +7,54 @@ fun main(args: Array<String>) {
 }
 
 
-fun abc140d(n: Int, bList: Int, s: String): Any? {
+fun abc140d(n: Int, k: Int, s: String): Any? {
 
-    var max = 0
+    val initScore = countHappy(s)
 
-    for (i in s.toCharArray().indices) {
+    return Math.min(initScore + 2 * k, s.length - 1)
+}
 
+fun countHappy(s: String): Int {
+    var count = 0
+    for (i in s.indices) {
 
+        // 右端と左端は特別
+        if (i == 0) {
+
+            if (s[i] == 'L') {
+                continue
+            }
+
+            if (s.length < 2) {
+                continue
+            }
+
+            if (s[i + 1] == 'R') {
+                count++
+            }
+            continue
+        }
+        if (i == s.length - 1) {
+            if (s[i] == 'R') {
+                continue
+            }
+
+            if (s[i - 1] == 'L') {
+                count++
+            }
+            continue
+        }
+
+        if (s[i] == 'L') {
+            if (s[i] == s[i - 1]) {
+                count++
+            }
+        } else {
+            if (s[i] == s[i + 1]) {
+                count++
+            }
+        }
     }
 
-    return max
+    return count
 }
