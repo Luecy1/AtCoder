1c372ab047755bbac1652ceb79a0ebfc0aacf38d
luecy1 <kowaheno@gmail.com>
2019-09-19 20:07:00 +0900

    ABC 133Cを修正

--- src/main/java/abc130/133c.kt
+++ src/main/java/abc130/133c.kt
@@ -1,12 +1,25 @@
 fun main(args: Array<String>) {
 
-    val (l,r) = readLine()!!.split(" ").map(String::toLong)
+    val (l, r) = readLine()!!.split(" ").map(String::toLong)
 
-    println(func133c(l,r))
+    println(func133c(l, r))
 }
 
-fun func133c(n: Long, aList: Long): Any? {
+fun func133c(l: Long, r: Long): Any? {
 
+    if (2019 <= r - l) {
+        return 0
+    }
 
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    val rmin = Math.min(r, l + 2019 * 2)
+
+    var ans = Long.MAX_VALUE
+    for (i in l..rmin) {
+        for (j in (i + 1)..rmin) {
+            val x = (i * j) % 2019
+            ans = Math.min(ans, x)
+        }
+    }
+
+    return ans
 }
