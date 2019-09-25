215b3049c0f3c1ecb3f5df49306d4f6e222a9e4b
luecy1 <kowaheno@gmail.com>
2019-09-25 22:06:22 +0900

    ABC141　テストケースを追加

---
+++ src/test/java/abc140/ABC141Test.kt
@@ -0,0 +1,13 @@
+package abc140
+
+import abc141a
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ABC141Test {
+
+    @Test
+    fun abc141A() {
+        assertEquals("Cloudy", abc141a("Sunny"))
+    }
+}
